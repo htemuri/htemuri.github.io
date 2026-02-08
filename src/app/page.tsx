5acc0cd2a1cc23ce1b865da0d7dea61ead28057b
Harris Temuri <harris.temuri@gmail.com>
2026-02-08 17:50:10 -0500

    update intro text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,11 +18,11 @@ im on a sabbatical right now since sept 2025 but my job was having a bunch of ha
 
   const whoami = `Hey! I'm [1;36mHarris[0m.[0m
 
-I’m 26 and currently based in Gainesville, Georgia. I spent the last few years as a "wearer of many hats" in platform engineering - moving between linux sysadmin, Azure administration, and building internal applications/automations. Having hands-on experience with every layer of our stack naturally made me the go-to person for resolving critical breaks or unblocking complex technical hurdles.
+I’m a software engineer based in Gainesville, Georgia (and open to relocating anywhere in the US). I spent the last few years as a "wearer of many hats" in platform engineering - moving between linux sysadmin, Azure administration, and building internal applications/automations. Having hands-on experience with every layer of our stack naturally made me the go-to person for resolving critical breaks or unblocking complex technical hurdles.
 
-Right now, I’m on a sabbatical to get back to the basics. Even though I’ve spent years in the cloud, I’ve always been curious about what’s happening under those layers of abstraction. I'm currently spending my time deep-diving into computer science fundamentals: computer architecture, distributed systems, operating systems, and networking (all mostly through the lens of Go). A few years ago, I took a similar leave of absence (~3 months) to study Linux and networking in depth; it fundamentally changed how I worked when I returned, and I’m spending this stint building that same level of mastery for the next chapter of my career (for which I'm open to relocating anywhere in the US).
+Right now, I’m on a sabbatical to get back to the basics. Even though I’ve spent years in the cloud, I’ve always been curious about what’s happening under those layers of abstraction. I'm currently spending my time deep-diving into computer science fundamentals: computer architecture, distributed systems, operating systems, and networking (all mostly through the lens of Go). A few years ago, I took a similar leave of absence (~3 months) to study Linux and networking in depth; it fundamentally changed how I worked when I returned, and I’m spending this stint building that same level of mastery for the next chapter of my career.
 
-When I’m not in a terminal, I’m known as a serial hobbyist, haha. I mainly love to lift, and at the moment I also dabble in woodworking, drumming, reading fantasy, sewing, and audiophile stuff (designed and built out my dad's home theater 🙂).
+When I’m not in a terminal, I’m a bit of a serial hobbyist, haha. I love to lift, but at the moment I also dabble in woodworking, drumming, reading fantasy, sewing, and audiophile stuff (designed and built out my dad's home theater 🙂).
   
 Thanks for visiting!
 
