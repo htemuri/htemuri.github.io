37c309480afd4e5256479458fe7caf5b2fdea40c
Harris Temuri <harris.temuri@gmail.com>
2026-02-08 14:00:22 -0500

    update intro text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,19 +12,24 @@ export default function Home() {
 
 [1m[1;32m$[0m whoami[0m
 `;
-  const whoami = `Hi, I’m [1;36mHarris[0m![0m
+  /*
+im on a sabbatical right now since sept 2025 but my job was having a bunch of hats related to platform engineering - i was a sysadmin, a mentor for a newly starting sysadmin, the networking sme, the everything troubleshooting guy (vms/azure app services,anything that could break in azure), i wrote some applications that showed customer metrics/status page/rest api for active directory, i also explored new technology like k8s/argo/helm workflows, also wrote cicd pipelines for all my apps.
+*/
 
-I tend to approach engineering as a long process of learning rather than a fixed skill set. I’m most interested in computer science fundamentals - the ideas and mechanisms that stay useful even as tools, technologies, and roles change.
+  const whoami = `Hey! I'm [1;36mHarris[0m.[0m
 
-That focus has shaped how I think about systems, how I approach new problems, and how I adapt to different kinds of work over time.
+I’m 26 and currently based in Gainesville, Georgia. I spent the last few years as a "wearer of many hats" in platform engineering - moving between linux sysadmin, Azure administration, and building internal applications/automations. Having hands-on experience with every layer of our stack naturally made me the go-to person for resolving critical breaks or unblocking complex technical hurdles.
 
-Please feel free to check out my experience in working as a contractor at the CDC in the [1;36mCareer[0m tab, and if you're still interested, check out my more systems-focused solo projects that I've worked on in the [1;36mProjects[0m tab.
+Right now, I’m on a sabbatical to get back to the basics. Even though I’ve spent years in the cloud, I’ve always been curious about what’s happening under those layers of abstraction. I'm currently spending my time deep-diving into computer science fundamentals: computer architecture, distributed systems, operating systems, and networking (all mostly through the lens of Go). A few years ago, I took a similar leave of absence (~3 months) to study Linux and networking in depth; it fundamentally changed how I worked when I returned, and I’m spending this stint building that same level of mastery for the next chapter of my career (for which I'm open to relocating anywhere in the US).
 
+When I’m not in a terminal, I’m known as a serial hobbyist, haha. I mainly love to lift, and at the moment I also dabble in woodworking, drumming, reading fantasy, sewing, and audiophile stuff (designed and built out my dad's home theater 🙂).
+  
 Thanks for visiting!
 
 [38;5;214m  |\\__/,|[0m   [38;5;214m(\`\\[0m
 [38;5;214m_.|[32mo[0m [32mo[0m  [38;5;214m|_[0m   [38;5;214m) )[0m
 [38;5;239m---------------------[0m
+
 `;
 
   return (
