975c876e2b9cc0cb2e0004041315f7511334c084
Harris Temuri <harris.temuri@gmail.com>
2026-06-20 14:27:23 -0400

    remove extra heading in intro text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,7 @@ import { useState } from "react";
 export default function Home() {
   const [showWhoami, setShowWhoami] = useState(false);
 
-  const introductionCode = `[2mStarting session…[0m
-[1;32mConnected.[0m
-
-[1m[1;32m$[0m whoami[0m
+  const introductionCode = `[1m[1;32m$[0m whoami[0m
 `;
   /*
 im on a sabbatical right now since sept 2025 but my job was having a bunch of hats related to platform engineering - i was a sysadmin, a mentor for a newly starting sysadmin, the networking sme, the everything troubleshooting guy (vms/azure app services,anything that could break in azure), i wrote some applications that showed customer metrics/status page/rest api for active directory, i also explored new technology like k8s/argo/helm workflows, also wrote cicd pipelines for all my apps.
